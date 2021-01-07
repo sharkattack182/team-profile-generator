7707a63396eda319480acbcc555027fa2b43a9aa
Matthew Courtney <matthewdc182@gmail.com>
2021-01-06 21:02:45 -0500

    got the management prompt workng and returning correctly

--- index.js
+++ index.js
@@ -4,73 +4,79 @@ const fs = require("fs");
 const team = [];
 
 const managerQuestions = [
-    {
-      type: "input",
-      message: "Who is the manager of this team?",
-      name: "manager",
-    },
-    {
-      type: "input",
-      message: "What is the managers email?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "What is the managers office number?",
-      name: "office",
-    },
-    {
-        type: "input",
-        message: "Do you have any team members?",
-        name: "teamConfirm",
-      },
-  ];
+  {
+    type: "input",
+    message: "Who is the manager of this team?",
+    name: "manager",
+  },
+  {
+    type: "input",
+    message: "What is the managers email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is the managers office number?",
+    name: "office",
+  },
+  {
+    type: "confirm",
+    message: "Do you have any team members?",
+    name: "teamConfirm",
+  },
+];
 
 const employeeQuestions = [
-    {
-      type: "input",
-      message: "What is the employees name?",
-      name: "name",
-    },
-    {
-      type: "number",
-      message: "What is the employees ID?",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "What is the employees GitHub?",
-      name: "github",
-    },
-    {
-      type: "input",
-      message: "What is the employees email?",
-      name: "email",
-    },
-    {
-      type: "list",
-      message: "What is the employees title?",
-      choices: ["Engineer", "Intern"],
-      name: "title",
-    },
-    {
-      type: "confirm",
-      message: "Would you like to add another employee?",
-      name: "add",
-    },
-  ];
+  {
+    type: "input",
+    message: "What is the employees name?",
+    name: "name",
+  },
+  {
+    type: "number",
+    message: "What is the employees ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the employees GitHub?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is the employees email?",
+    name: "email",
+  },
+  {
+    type: "list",
+    message: "What is the employees title?",
+    choices: ["Engineer", "Intern"],
+    name: "title",
+  },
+  {
+    type: "confirm",
+    message: "Would you like to add another employee?",
+    name: "add",
+  },
+];
 
-  function teamListBuild() {
-      inquirer.prompt(employeeQuestions).then(data => {
-        console.log(data)
-        if(data.add === true) {
-            team.push(data);
-            teamListBuild();
-        }
-        else{
-            console.log(team)
-        }
-      })
-  }
+function teamListBuild() {
+  inquirer.prompt(employeeQuestions).then((data) => {
+    console.log(data);
+    if (data.add === true) {
+      team.push(data);
+      teamListBuild();
+    } else {
+      console.log(team);
+    }
+  });
+}
 
-  teamListBuild();
+function managerInit() {
+    inquirer.prompt(managerQuestions).then(response => {
+        console.log(response)
+    })
+}
+
+managerInit();
+// teamListBuild();
